d057f269f1a5ca54ef0a3982a3818664dbd432cb
Courtney Smith <smith.courtney@berkeley.edu>
2022-01-30 20:33:05 -0800

    updating cam.py and listener.py

--- camera/cam.py
+++ camera/cam.py
@@ -1,23 +1,39 @@
 # this is from https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
 import numpy as np
-import cv2
-
-
+import cv2 as cv
+import paho.mqtt.client as mqtt
 # the index depends on your camera setup and which one is your USB camera.
-cap = cv2.VideoCapture(0)
+LOCAL_MQTT_HOST="172.16.24.2"
+LOCAL_MQTT_PORT=31044
+LOCAL_MQTT_TOPIC="test_topic"
 
+def on_connect_local(client, userdata, flags, rc):
+        print("connected to local broker with rc: " + str(rc))
+
+local_mqttclient = mqtt.Client()
+local_mqttclient.on_connect = on_connect_local
+local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cap = cv.VideoCapture(0)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        rc,png = cv.imencode('.png', gray)
+        msg = png.tobytes()
 
+    #publish the message
+        local_mqttclient.publish(LOCAL_MQTT_TOPIC, msg)
+        print('msg received')
+    
     # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv.imshow('frame',gray)
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
-
 # When everything done, release the capture
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
--- listener/listener.py
+++ listener/listener.py
@@ -3,18 +3,25 @@ import paho.mqtt.client as mqtt
 
 LOCAL_MQTT_HOST="172.16.24.2"
 LOCAL_MQTT_PORT=31044
-LOCAL_MQTT_TOPIC="test_topic"
+LOCAL_MQTT_TOPIC="image_topic"
+REMOTE_MQTT_HOST="172.31.94.10"
+REMOTE_MQTT_PORT="31857"
+REMOTE_MQTT_TOPIC="image_topic_remote"
 
 def on_connect_local(client, userdata, flags, rc):
         print("connected to local broker with rc: " + str(rc))
         client.subscribe(LOCAL_MQTT_TOPIC)
+        
+def on_connect_remote(client, userdata, flags, rc):
+        print("connected to remote broker with rc: " + str(rc))
+        client.subscribe(REMOTE_MQTT_TOPIC)
     
 def on_message(client,userdata, msg):
   try:
     print("message received: ",str(msg.payload.decode("utf-8")))
     # if we wanted to re-publish this message, something like this should work
-    # msg = msg.payload
-    # remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
+    msg = msg.payload
+    remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
   except:
     print("Unexpected error:", sys.exc_info()[0])
 
@@ -23,7 +30,10 @@ local_mqttclient.on_connect = on_connect_local
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
 local_mqttclient.on_message = on_message
 
-
+remote_mqttclient = mqtt.Client()
+remote_mqttclient.on_connect = on_connect_remote
+remote_mqttclient.connect(REMOTE_MQTT_HOST, REMOTE_MQTT_PORT, 60)
+remote_mqttclient.onmessage = on_message
 
 # go into a loop
 local_mqttclient.loop_forever()
