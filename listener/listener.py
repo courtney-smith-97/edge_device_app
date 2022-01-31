7fac2a55e36e20874c92fd8e346c4452295f69d6
Courtney Smith <75190843+courtney-smith-97@users.noreply.github.com>
2022-01-31 16:33:01 -0500

    Update listener.py

--- listener/listener.py
+++ listener/listener.py
@@ -5,7 +5,7 @@ LOCAL_MQTT_HOST="172.16.24.2"
 LOCAL_MQTT_PORT=32153
 LOCAL_MQTT_TOPIC="image_topic"
 REMOTE_MQTT_HOST="172.31.94.10"
-REMOTE_MQTT_PORT=31857
+REMOTE_MQTT_PORT=30835
 REMOTE_MQTT_TOPIC="image_topic_remote"
 
 def on_connect_local(client, userdata, flags, rc):
@@ -28,12 +28,12 @@ def on_message(client,userdata, msg):
 local_mqttclient = mqtt.Client()
 local_mqttclient.on_connect = on_connect_local
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
-local_mqttclient.on_message = on_message
 
 remote_mqttclient = mqtt.Client()
 remote_mqttclient.on_connect = on_connect_remote
 remote_mqttclient.connect(REMOTE_MQTT_HOST, REMOTE_MQTT_PORT, 60)
-remote_mqttclient.onmessage = on_message
+
+local_mqttclient.on_message = on_message
 
 # go into a loop
 remote_mqttclient.loop_start()
