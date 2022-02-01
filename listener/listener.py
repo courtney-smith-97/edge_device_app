aef70e5e2eaf148b18f46cf002763848e7f9f292
Courtney Smith <75190843+courtney-smith-97@users.noreply.github.com>
2022-01-31 19:34:11 -0500

    Update listener.py

--- listener/listener.py
+++ listener/listener.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-
+import sys
 
 LOCAL_MQTT_HOST="172.16.24.2"
 LOCAL_MQTT_PORT=32258
@@ -18,7 +18,7 @@ def on_connect_remote(client, userdata, flags, rc):
     
 def on_message(client,userdata, msg):
   try:
-    print("message received: ",str(msg.payload.decode("utf-8")))
+    print("message received")
     # if we wanted to re-publish this message, something like this should work
     msg = msg.payload
     remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
